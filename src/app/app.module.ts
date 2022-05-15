20582cc1cd5bc928a2faecbc621afea6c540d2ce
hasanibishi <juled.ibishi@hotmail.com>
2022-05-15 17:10:17 +0200

    header,home,plans are completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlansComponent } from './components/plans/plans.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlansComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
