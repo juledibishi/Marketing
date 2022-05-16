c1d4ea100d4fff28affc9aff586c793ed30d35b2
hasanibishi <juled.ibishi@hotmail.com>
2022-05-16 21:01:43 +0200

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PlansComponent } from './components/plans/plans.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-home', component: HomeComponent },
+  { path: 'app-about', component: AboutComponent },
+  { path: 'app-plans', component: PlansComponent },
+  { path: 'app-contact', component: ContactComponent },
+  { path: '**', redirectTo: 'app-home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
